3daa546358a6cf6e0fccf27112f2169480c905f3
crispytwig <48872606+crispytwig@users.noreply.github.com>
2026-04-25 18:21:07 -0500

    Fix Snake sleeping textures

--- src/main/java/com/starfish_studios/naturalist/client/renderer/SnakeRenderer.java
+++ src/main/java/com/starfish_studios/naturalist/client/renderer/SnakeRenderer.java
@@ -1,13 +1,10 @@
 package com.starfish_studios.naturalist.client.renderer;
 
-import com.starfish_studios.naturalist.Naturalist;
 import com.starfish_studios.naturalist.client.model.SnakeModel;
-import com.starfish_studios.naturalist.client.renderer.layers.SleepLayer;
 import com.starfish_studios.naturalist.server.entity.mob.Snake;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -16,7 +13,6 @@ public class SnakeRenderer extends GeoEntityRenderer<Snake> {
     public SnakeRenderer(EntityRendererProvider.@NotNull Context renderManager) {
         super(renderManager, new SnakeModel());
         this.shadowRadius = 0.4F;
-        this.addRenderLayer(new SleepLayer<>(this, ResourceLocation.fromNamespaceAndPath(Naturalist.MOD_ID, "textures/entity/snake/snake.png")));
     }
 
     @SuppressWarnings("unused")
