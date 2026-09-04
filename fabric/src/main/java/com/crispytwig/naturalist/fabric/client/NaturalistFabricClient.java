666d92f5b38b878b0b5ba6abc974efd5bf23886b
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-04 15:48:59 +0200

    Remove obsolete manual 26.2 chunk render layers

--- fabric/src/main/java/com/crispytwig/naturalist/fabric/client/NaturalistFabricClient.java
+++ fabric/src/main/java/com/crispytwig/naturalist/fabric/client/NaturalistFabricClient.java
@@ -5,15 +5,11 @@ import com.crispytwig.naturalist.NaturalistClientConfig;
 import com.crispytwig.naturalist.fabric.config.FabricNaturalistClientConfig;
 import com.crispytwig.naturalist.client.particle.CaptureNetSwingParticle;
 import com.crispytwig.naturalist.registry.NaturalistParticleTypes;
-import com.crispytwig.naturalist.registry.NaturalistRegistry;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.ModelLayerRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-
 
 public class NaturalistFabricClient implements ClientModInitializer {
     @Override
@@ -28,22 +24,6 @@ public class NaturalistFabricClient implements ClientModInitializer {
         NaturalistClient.registerItemProperties();
         NaturalistClient.registerMenuScreens(MenuScreens::register);
 
-
         ParticleProviderRegistry.getInstance().register(NaturalistParticleTypes.CAPTURE_NET_SWING.get(), CaptureNetSwingParticle.Provider::new);
-
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.ORANGE_STARFISH.get(), ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.PURPLE_STARFISH.get(), ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.BLUE_STARFISH.get(), ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.RED_STARFISH.get(), ChunkSectionLayer.CUTOUT);
-
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.SNAIL_EGGS.get(), ChunkSectionLayer.CUTOUT);
-
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.AZURE_FROGLASS.get(), ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.VERDANT_FROGLASS.get(), ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.CRIMSON_FROGLASS.get(), ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.AZURE_FROGLASS_PANE.get(), ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.VERDANT_FROGLASS_PANE.get(), ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(NaturalistRegistry.CRIMSON_FROGLASS_PANE.get(), ChunkSectionLayer.TRANSLUCENT);
     }
-
 }
