4d8b11b0aab594cd976fee8913a0e3bcb8070a74
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-04 23:03:59 +0200

    Add real singleplayer client gametest

---
+++ src/gametest/java/com/crispytwig/naturalist/test/NaturalistClientGameTest.java
@@ -0,0 +1,18 @@
+package com.crispytwig.naturalist.test;
+
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
+import net.fabricmc.fabric.api.client.gametest.v1.context.TestSingleplayerContext;
+
+@SuppressWarnings("UnstableApiUsage")
+public final class NaturalistClientGameTest implements FabricClientGameTest {
+    public static final String WORLD_JOINED_MARKER = "NATURALIST_CLIENT_GAMETEST_WORLD_JOINED";
+
+    @Override
+    public void runTest(ClientGameTestContext context) {
+        try (TestSingleplayerContext singleplayer = context.worldBuilder().create()) {
+            singleplayer.getClientLevel().waitForChunksRender();
+            System.out.println(WORLD_JOINED_MARKER);
+        }
+    }
+}
