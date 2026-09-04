9f75910e9087c79df2216446c66aa0e8a4a2b12f
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-04 14:32:15 +0200

    Port Bass render type to 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/model/BassModel.java
+++ common/src/main/java/com/crispytwig/naturalist/client/model/BassModel.java
@@ -11,7 +11,7 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
-import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.util.Mth;
 import org.jspecify.annotations.NonNull;
 
@@ -22,7 +22,7 @@ public class BassModel extends NaturalistEntityModel<Bass> {
 	private final ModelPart body;
 
 	public BassModel(ModelPart root) {
-		super(root.getChild("root"), RenderType::entityCutout);
+		super(root.getChild("root"), RenderTypes::entityCutoutCull);
 		this.root = root.getChild("root");
 		this.body = this.root.getChild("body");
 	}
