19e94fbf194993ef8065dbd454abaa82ef968af5
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 13:34:38 +0200

    Restore wolf mole-dig render state on 26.2

---
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/state/NaturalistWolfRenderState.java
@@ -0,0 +1,7 @@
+package com.crispytwig.naturalist.client.renderer.state;
+
+public interface NaturalistWolfRenderState {
+    boolean naturalist$isDiggingOutMole();
+
+    void naturalist$setDiggingOutMole(boolean digging);
+}
