bc354e28f11e0f97b08e0e000dec7033d705a4a6
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:28:32 +0200

    Restore lizard dye render layer on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/LizardRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/LizardRenderer.java
@@ -1,11 +1,11 @@
 package com.crispytwig.naturalist.client.renderer;
 
 import com.crispytwig.naturalist.client.model.LizardModel;
+import com.crispytwig.naturalist.client.renderer.layers.DyeLayer;
 import com.crispytwig.naturalist.server.entity.mob.Lizard;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -13,5 +13,6 @@ import org.jetbrains.annotations.NotNull;
 public class LizardRenderer extends NaturalistSingleMobRenderer<Lizard> {
     public LizardRenderer(EntityRendererProvider.@NotNull Context context) {
         super(context, new LizardModel(context.bakeLayer(LizardModel.LAYER_LOCATION)), 0.4F);
+        this.addLayer(new DyeLayer<>(this, "lizard"));
     }
 }
