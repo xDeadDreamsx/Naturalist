a9137a7f1155786904d26831dd9d12db8b46cbb6
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:28:42 +0200

    Restore lizard tail render offset on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/LizardTailRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/LizardTailRenderer.java
@@ -1,12 +1,12 @@
 package com.crispytwig.naturalist.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.crispytwig.naturalist.client.model.LizardTailModel;
+import com.crispytwig.naturalist.client.renderer.state.NaturalistRenderState;
 import com.crispytwig.naturalist.server.entity.mob.LizardTail;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -14,5 +14,10 @@ import org.jetbrains.annotations.NotNull;
 public class LizardTailRenderer extends NaturalistSingleMobRenderer<LizardTail> {
     public LizardTailRenderer(EntityRendererProvider.Context context) {
         super(context, new LizardTailModel(context.bakeLayer(LizardTailModel.LAYER_LOCATION)), 0.4F);
+    }
+
+    @Override
+    protected void scale(@NotNull NaturalistRenderState<LizardTail> state, @NotNull PoseStack poseStack) {
+        poseStack.translate(0.0F, -0.3F, 0.0F);
     }
 }
