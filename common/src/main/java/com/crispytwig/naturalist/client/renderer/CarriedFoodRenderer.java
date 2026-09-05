b449786e72e46ccc061e409044e9949c2558efe4
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:19:49 +0200

    Restore ant carried-food render positioning on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/CarriedFoodRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/CarriedFoodRenderer.java
@@ -2,11 +2,11 @@ package com.crispytwig.naturalist.client.renderer;
 
 import com.crispytwig.naturalist.server.entity.misc.CarriedFoodEntity;
 import com.crispytwig.naturalist.server.entity.mob.Ant;
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemEntityRenderer;
+import net.minecraft.client.renderer.entity.state.ItemEntityRenderState;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.item.ItemEntity;
 
@@ -16,5 +16,18 @@ public class CarriedFoodRenderer extends ItemEntityRenderer {
     public CarriedFoodRenderer(EntityRendererProvider.Context context) {
         super(context);
         this.shadowRadius = 0.0F;
+    }
+
+    @Override
+    public void extractRenderState(ItemEntity entity, ItemEntityRenderState state, float partialTick) {
+        super.extractRenderState(entity, state, partialTick);
+        if (entity instanceof CarriedFoodEntity food) {
+            Ant ant = food.resolveAnt();
+            if (ant != null) {
+                state.x = Mth.lerp(partialTick, ant.xOld, ant.getX());
+                state.y = Mth.lerp(partialTick, ant.yOld, ant.getY()) + ant.getBbHeight() + CarriedFoodEntity.BACK_GAP;
+                state.z = Mth.lerp(partialTick, ant.zOld, ant.getZ());
+            }
+        }
     }
 }
