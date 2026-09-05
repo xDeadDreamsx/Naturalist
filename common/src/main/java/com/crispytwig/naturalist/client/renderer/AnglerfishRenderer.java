cd919b0e709613661fb48822d61465988fdc2f4f
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:29:34 +0200

    Restore full-bright glowing anglerfish on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/AnglerfishRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/AnglerfishRenderer.java
@@ -1,14 +1,14 @@
 package com.crispytwig.naturalist.client.renderer;
 
 import com.crispytwig.naturalist.client.renderer.state.NaturalistRenderState;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import com.crispytwig.naturalist.client.model.AnglerfishModel;
 import com.crispytwig.naturalist.server.entity.mob.Anglerfish;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.Identifier;
+import net.minecraft.core.BlockPos;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -19,10 +19,22 @@ public class AnglerfishRenderer extends NaturalistSingleMobRenderer<Anglerfish> 
     }
 
     @Override
+    protected int getBlockLightLevel(@NotNull Anglerfish entity, @NotNull BlockPos pos) {
+        return entity.isGlowing() ? 15 : super.getBlockLightLevel(entity, pos);
+    }
+
+    @Override
+    protected int getSkyLightLevel(@NotNull Anglerfish entity, @NotNull BlockPos pos) {
+        return entity.isGlowing() ? 15 : super.getSkyLightLevel(entity, pos);
+    }
+
+    @Override
     protected void setupRotations(@NotNull NaturalistRenderState<Anglerfish> state, @NotNull PoseStack poseStack, float yBodyRot, float nativeScale) {
         Anglerfish entity = state.entity;
         float partialTick = state.partialTick;
         super.setupRotations(state, poseStack, yBodyRot, nativeScale);
-        poseStack.mulPose(Axis.ZP.rotationDegrees(-entity.swimTilt.getTilt(partialTick)));
+        if (entity != null) {
+            poseStack.mulPose(Axis.ZP.rotationDegrees(-entity.swimTilt.getTilt(partialTick)));
+        }
     }
 }
