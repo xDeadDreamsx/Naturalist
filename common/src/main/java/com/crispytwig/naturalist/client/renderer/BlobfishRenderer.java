f4828ad68e828aea6cb53c1faa8ae35af9040ea9
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:29:08 +0200

    Restore gray blobfish model selection on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/BlobfishRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/BlobfishRenderer.java
@@ -1,11 +1,13 @@
 package com.crispytwig.naturalist.client.renderer;
 
+import com.crispytwig.naturalist.Naturalist;
+import com.crispytwig.naturalist.client.model.BlobfishGrayModel;
+import com.crispytwig.naturalist.client.model.BlobfishPinkModel;
+import com.crispytwig.naturalist.client.model.NaturalistEntityModel;
 import com.crispytwig.naturalist.client.renderer.state.NaturalistRenderState;
+import com.crispytwig.naturalist.server.entity.mob.Blobfish;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import com.crispytwig.naturalist.Naturalist;
-import com.crispytwig.naturalist.client.model.BlobfishPinkModel;
-import com.crispytwig.naturalist.server.entity.mob.Blobfish;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -18,13 +20,27 @@ public class BlobfishRenderer extends NaturalistSingleMobRenderer<Blobfish> {
     private static final Identifier GRAY = Naturalist.location("textures/entity/blobfish/gray.png");
     private static final Identifier PINK = Naturalist.location("textures/entity/blobfish/pink.png");
 
+    private final NaturalistEntityModel<Blobfish> pinkModel;
+    private final NaturalistEntityModel<Blobfish> grayModel;
+
     public BlobfishRenderer(EntityRendererProvider.Context context) {
         super(context, new BlobfishPinkModel(context.bakeLayer(BlobfishPinkModel.LAYER_LOCATION)), 0.0F);
+        this.pinkModel = this.model;
+        this.grayModel = new BlobfishGrayModel(context.bakeLayer(BlobfishGrayModel.LAYER_LOCATION));
+    }
+
+    @Override
+    public void extractRenderState(Blobfish entity, NaturalistRenderState<Blobfish> state, float partialTick) {
+        this.model = entity.isGray() ? this.grayModel : this.pinkModel;
+        super.extractRenderState(entity, state, partialTick);
     }
 
     @Override
     public @NotNull Identifier getTextureLocation(@NotNull NaturalistRenderState<Blobfish> state) {
         Blobfish entity = state.entity;
+        if (entity == null) {
+            return PINK;
+        }
         if (entity.hasNonDefaultVariant()) {
             return entity.getVariantTexture();
         }
@@ -36,6 +52,8 @@ public class BlobfishRenderer extends NaturalistSingleMobRenderer<Blobfish> {
         Blobfish entity = state.entity;
         float partialTick = state.partialTick;
         super.setupRotations(state, poseStack, yBodyRot, nativeScale);
-        poseStack.mulPose(Axis.ZP.rotationDegrees(-entity.swimTilt.getTilt(partialTick)));
+        if (entity != null) {
+            poseStack.mulPose(Axis.ZP.rotationDegrees(-entity.swimTilt.getTilt(partialTick)));
+        }
     }
 }
