71c41a851f70fb3c1946c9a14bfd76b73eef13c5
Naturalist 26.2 Port CI <actions@users.noreply.github.com>
2026-09-05 12:20:01 +0000

    Apply Minecraft 26.2 API migration pass

--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
@@ -409,6 +409,8 @@ public class Hedgehog extends TamableAnimal implements DyeableAnimal, FollowingP
 
 
 
+
+
     @Override
     public void tame(@NotNull Player player) {
         super.tame(player);
--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Snake.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Snake.java
@@ -546,6 +546,8 @@ public class Snake extends TamableClimbingAnimal implements SleepingAnimal, Neut
 
 
 
+
+
     @Nullable
     @Override
     protected SoundEvent getAmbientSound() {
