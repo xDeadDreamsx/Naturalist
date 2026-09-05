b53b1b50364d219711f72c708e35b6997fb37906
Naturalist 26.2 Port CI <actions@users.noreply.github.com>
2026-09-05 12:17:36 +0000

    Apply Minecraft 26.2 API migration pass

--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
@@ -405,6 +405,8 @@ public class Hedgehog extends TamableAnimal implements DyeableAnimal, FollowingP
 
 
 
+
+
     @Override
     public void tame(@NotNull Player player) {
         super.tame(player);
