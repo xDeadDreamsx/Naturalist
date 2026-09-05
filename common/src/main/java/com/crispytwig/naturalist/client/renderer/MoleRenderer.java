179852a41ee1b3564f8fb683e64e35a2f145b1a8
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:20:41 +0200

    Restore mole baby scaling and rolled-up shadow behavior on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/MoleRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/MoleRenderer.java
@@ -1,18 +1,32 @@
 package com.crispytwig.naturalist.client.renderer;
 
 import com.crispytwig.naturalist.client.model.MoleModel;
+import com.crispytwig.naturalist.client.renderer.state.NaturalistRenderState;
 import com.crispytwig.naturalist.server.entity.mob.Mole;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.Identifier;
-import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 @Environment(EnvType.CLIENT)
 public class MoleRenderer extends NaturalistSingleMobRenderer<Mole> {
     public MoleRenderer(EntityRendererProvider.Context context) {
         super(context, new MoleModel(context.bakeLayer(MoleModel.LAYER_LOCATION)), 0.4F);
+    }
+
+    @Override
+    protected void scale(NaturalistRenderState<Mole> state, PoseStack poseStack) {
+        super.scale(state, poseStack);
+        if (state.isBaby) {
+            poseStack.scale(0.6F, 0.6F, 0.6F);
+        }
+    }
+
+    @Override
+    protected float getShadowRadius(NaturalistRenderState<Mole> state) {
+        Mole entity = state.entity;
+        if (entity != null && entity.isRolledUp()) return 0.0F;
+        return state.isBaby ? 0.25F : 0.4F;
     }
 }
