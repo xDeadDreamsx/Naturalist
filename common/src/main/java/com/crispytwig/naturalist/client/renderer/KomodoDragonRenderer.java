fdd2cc5532eee59845357524f6eb1946d6090d5b
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 14:28:25 +0200

    Restore Komodo dragon baby render parity on 26.2

--- common/src/main/java/com/crispytwig/naturalist/client/renderer/KomodoDragonRenderer.java
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/KomodoDragonRenderer.java
@@ -1,12 +1,12 @@
 package com.crispytwig.naturalist.client.renderer;
 
 import com.crispytwig.naturalist.client.model.KomodoDragonModel;
+import com.crispytwig.naturalist.client.renderer.state.NaturalistRenderState;
 import com.crispytwig.naturalist.server.entity.mob.KomodoDragon;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.resources.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
@@ -14,5 +14,17 @@ import org.jetbrains.annotations.NotNull;
 public class KomodoDragonRenderer extends NaturalistSingleMobRenderer<KomodoDragon> {
     public KomodoDragonRenderer(EntityRendererProvider.Context context) {
         super(context, new KomodoDragonModel(context.bakeLayer(KomodoDragonModel.LAYER_LOCATION)), 0.65F);
+    }
+
+    @Override
+    protected void scale(@NotNull NaturalistRenderState<KomodoDragon> state, @NotNull PoseStack poseStack) {
+        if (state.entity != null && state.entity.isBaby()) {
+            poseStack.scale(0.45F, 0.45F, 0.45F);
+        }
+    }
+
+    @Override
+    protected float getShadowRadius(@NotNull NaturalistRenderState<KomodoDragon> state) {
+        return state.entity != null && state.entity.isBaby() ? 0.3F : 0.65F;
     }
 }
