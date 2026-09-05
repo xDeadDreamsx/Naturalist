5eac178807a830953a4f0f4887895aef94625636
Naturalist 26.2 Port CI <actions@users.noreply.github.com>
2026-09-05 11:41:44 +0000

    Apply Minecraft 26.2 API migration pass

--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Hedgehog.java
@@ -399,6 +399,8 @@ public class Hedgehog extends TamableAnimal implements DyeableAnimal, FollowingP
 
 
 
+
+
     @Override
     public void tame(@NotNull Player player) {
         super.tame(player);
--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Ostrich.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Ostrich.java
@@ -504,6 +504,8 @@ public class Ostrich extends TamableAnimal implements EggLayingAnimal, HidingAni
 
 
 
+
+
     @Override
     public @NotNull InteractionResult mobInteract(@NotNull Player player, @NotNull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
@@ -739,6 +741,7 @@ public class Ostrich extends TamableAnimal implements EggLayingAnimal, HidingAni
             this.stopBeingAngry();
         }
     }
+
 
 
 
