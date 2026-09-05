80231edea0f0402b52f6f4c1519cbf286c8f348a
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 13:44:35 +0200

    Add Naturalist shoulder-flight avatar render state

---
+++ common/src/main/java/com/crispytwig/naturalist/client/renderer/state/NaturalistAvatarRenderState.java
@@ -0,0 +1,7 @@
+package com.crispytwig.naturalist.client.renderer.state;
+
+public interface NaturalistAvatarRenderState {
+    boolean naturalist$shouldShoulderParrotsFlap();
+
+    void naturalist$setShoulderParrotsFlap(boolean flap);
+}
