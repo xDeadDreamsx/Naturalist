5b42d2afe886dab10eedbbd861479b0d06648df0
Naturalist 26.2 Port CI <actions@users.noreply.github.com>
2026-09-05 13:07:15 +0000

    Apply Minecraft 26.2 API migration pass

--- common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Ostrich.java
+++ common/src/main/java/com/crispytwig/naturalist/server/entity/mob/Ostrich.java
@@ -538,6 +538,8 @@ public class Ostrich extends TamableAnimal implements EggLayingAnimal, HidingAni
 
 
 
+
+
     @Override
     public @NotNull InteractionResult mobInteract(@NotNull Player player, @NotNull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
@@ -773,6 +775,7 @@ public class Ostrich extends TamableAnimal implements EggLayingAnimal, HidingAni
             this.stopBeingAngry();
         }
     }
+
 
 
 
