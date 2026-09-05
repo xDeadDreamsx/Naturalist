2dafcb45d2582d7ba4f9040a8be7136b6c1debdf
xDeadDreamsx <jasonraufmann@gmail.com>
2026-09-05 13:44:56 +0200

    Extract shoulder-flight state for avatar rendering

---
+++ common/src/main/java/com/crispytwig/naturalist/mixin/AvatarRendererMixin.java
@@ -0,0 +1,22 @@
+package com.crispytwig.naturalist.mixin;
+
+import com.crispytwig.naturalist.client.renderer.state.NaturalistAvatarRenderState;
+import com.crispytwig.naturalist.server.entity.util.ParrotFlight;
+import net.minecraft.client.entity.ClientAvatarEntity;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.client.renderer.entity.state.AvatarRenderState;
+import net.minecraft.world.entity.Avatar;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AvatarRenderer.class)
+public abstract class AvatarRendererMixin {
+    @Inject(method = "extractRenderState", at = @At("TAIL"))
+    private <T extends Avatar & ClientAvatarEntity> void naturalist$extractShoulderFlight(
+            T entity, AvatarRenderState state, float partialTicks, CallbackInfo ci) {
+        ((NaturalistAvatarRenderState) state).naturalist$setShoulderParrotsFlap(
+                ParrotFlight.hasBirdOnHead(entity) && !entity.onGround());
+    }
+}
